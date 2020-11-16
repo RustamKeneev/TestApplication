52a8f9365618904d8fa20717d3640e871164fc80
rustamkeneev <dr.rustamkeneev@gmail.com>
2020-11-17 01:17:30 +0600

    findById views to VerifyCodeActivity

--- app/src/main/java/com/onlineapteka/testapplication/login/VerifyCodeActivity.java
+++ app/src/main/java/com/onlineapteka/testapplication/login/VerifyCodeActivity.java
@@ -3,14 +3,35 @@ package com.onlineapteka.testapplication.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.onlineapteka.testapplication.R;
 
 public class VerifyCodeActivity extends AppCompatActivity {
 
+    private EditText mCodeVerifyEdit;
+    private Button mButtonBack;
+    private Button mVerifyCodeButton;
+    private ProgressBar mVerifyProgressBar;
+    private Button mNextButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_code);
+        initViews();
+    }
+
+    private void initViews() {
+        mCodeVerifyEdit = findViewById(R.id.code_verify_edit);
+        mButtonBack = findViewById(R.id.button_back);
+        mVerifyCodeButton = findViewById(R.id.verify_code_button);
+        mVerifyProgressBar = findViewById(R.id.verify_progress_bar);
+        mNextButton = findViewById(R.id.next_button);
+
+
     }
 }
