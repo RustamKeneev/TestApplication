1f83e719d3604dd66b1ef9ccff5cffa7da74b933
rustamkeneev <dr.rustamkeneev@gmail.com>
2020-11-17 01:11:19 +0600

    update verify activity xml

--- app/src/main/java/com/onlineapteka/testapplication/login/PhoneAuthActivity.java
+++ app/src/main/java/com/onlineapteka/testapplication/login/PhoneAuthActivity.java
@@ -2,15 +2,36 @@ package com.onlineapteka.testapplication.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.onlineapteka.testapplication.R;
 
 public class PhoneAuthActivity extends AppCompatActivity {
 
+    private EditText mPhoneNumberEdit;
+    private Button mContinuePhoneNumberButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_auth);
+        initViews();
+    }
+
+    private void initViews() {
+        mPhoneNumberEdit = findViewById(R.id.phone_number_edit);
+        mContinuePhoneNumberButton = findViewById(R.id.continue_register_phone_button);
+        mContinuePhoneNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PhoneAuthActivity.this,VerifyCodeActivity.class));
+                finish();
+            }
+        });
     }
 }
