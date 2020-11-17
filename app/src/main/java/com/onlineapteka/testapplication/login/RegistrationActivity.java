18e21df4fd2fc62be8967421e7adabdaa00b4882
rustamkeneev <dr.rustamkeneev@gmail.com>
2020-11-17 21:18:24 +0600

    added start to activity

--- app/src/main/java/com/onlineapteka/testapplication/login/RegistrationActivity.java
+++ app/src/main/java/com/onlineapteka/testapplication/login/RegistrationActivity.java
@@ -2,6 +2,8 @@ package com.onlineapteka.testapplication.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,10 @@ public class RegistrationActivity extends AppCompatActivity {
     private Spinner mSexSpinner;
     private Spinner mAgeSpinner;
     private Button mVerifyRegisterButton;
+
+    public static void start(Context context){
+        context.startActivity(new Intent(context,RegistrationActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
